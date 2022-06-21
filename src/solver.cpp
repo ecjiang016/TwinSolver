3b92a68f9c56a46aa1e90b729efad95677edaca6
ecjiang016 <ecjiang016@gmail.com>
2022-06-20 22:12:59 -0500

    Changed to 2 phase solver

--- src/solver.cpp
+++ src/solver.cpp
@@ -1,52 +1,57 @@
 #include "cube.h"
+#include "patterns.h"
 #include <vector>
 
 //Based on https://en.wikipedia.org/wiki/Iterative_deepening_A*?scrlybrkr=b5bb56ed
 
 const unsigned int FOUND = 0xFFFFFF; //Big enough that it shouldn't ever be reached
-const Move allMoves[18] = {D, F, R, U, B, L, Dp, Fp, Rp, Up, Bp, Lp, D2, F2, R2, U2, B2, L2}; // Used to loop through all the moves
+const std::vector<Move> allMoves = {D, F, R, U, B, L, Dp, Fp, Rp, Up, Bp, Lp, D2, F2, R2, U2, B2, L2}; // Used to loop through all the moves
+const std::vector<Move> movesAfterG1 = {U, Up, U2, D, Dp, D2, F2, R2, B2, L2}; // A cube in G1 has the nice property that only those 10 moves are needed to solve the cube
 
-class Solver {
+enum CubeState { SOLVED, G1 };
+
+class SinglePhaseSolver {
   private:
-    unsigned int search(std::vector<Move> &path, unsigned int g, unsigned int bound, Cube cube);
-    unsigned int heuristic(Cube &cube);
+    Patterns<CornerHash, CORNER_PATTERNS_SIZE> corner_patterns;
+
+    template<CubeState cube_state> unsigned int search(std::vector<Move> &path, unsigned int g, unsigned int bound, Cube cube);
+    inline unsigned int heuristic(Cube &cube);
 
   public:
-    std::vector<Move> solve(Cube &cube);
+    SinglePhaseSolver() : corner_patterns("./databases/corner.patterns") {}
+    template<CubeState cube_state> std::vector<Move> solve(Cube &cube);
 };
 
-unsigned int Solver::heuristic(Cube &cube) {
-    return 0;
+inline unsigned int SinglePhaseSolver::heuristic(Cube &cube) {
+    return corner_patterns.getDepth(cube);
 }
 
-unsigned int Solver::search(std::vector<Move> &path, unsigned int g, unsigned int bound, Cube cube) {
+template<CubeState cube_state>
+unsigned int SinglePhaseSolver::search(std::vector<Move> &path, unsigned int g, unsigned int bound, Cube cube) {
     Move last_move = path.back();
     cube.rotate(last_move);
     unsigned int f = g + heuristic(cube);
 
-    if (cube.isSolved()) {
+    if (cube_state == SOLVED ? cube.isSolved() : cube.inG1()) {
         return FOUND;
     } else if (f > bound) {
         return f;
     }
     unsigned int min = 0xFFFF; // Set above upper bound
     
-    for (Move move : allMoves) {
+    std::vector<Move> branching_moves = cube_state == G1 ? allMoves : movesAfterG1; //Moves to consider
+    for (Move move : branching_moves) {
         //Pruning here
         if ((move & 0b00111) != (last_move & 0b00111)) { // There is no reason why you should move the same layer twice
             path.push_back(move);
-            //cube.rotate(move);
 
-            unsigned int t = search(path, g + 1, bound, cube);
+            unsigned int t = search<cube_state>(path, g + 1, bound, cube);
             if (t == FOUND) {
                 return FOUND;
             } else if (t < min) {
                 min = t;
             }
 
-            
-            //cube.rotate(Move((~((move & 0b10000) >> 1) & 0b01000) ^ move)); //Cool logic to get the reverse of the move
-            //cube.rotate(reverse_move(move));
             path.pop_back();
             
         }
@@ -54,14 +59,15 @@ unsigned int Solver::search(std::vector<Move> &path, unsigned int g, unsigned in
     return min;
 }
 
-std::vector<Move> Solver::solve(Cube &cube) {
+template<CubeState cube_state>
+std::vector<Move> SinglePhaseSolver::solve(Cube &cube) {
     unsigned int bound = heuristic(cube);
     std::vector<Move> path;
     path.reserve(35);
     path.push_back(NULL_MOVE); //Placeholder
 
     while (true) {
-        unsigned int t = search(path, 0U, bound, cube);
+        unsigned int t = search<cube_state>(path, 0U, bound, cube);
         if (t == FOUND) {
             path.erase(path.begin()); //Delete the placeholder
             return path;
@@ -74,8 +80,22 @@ std::vector<Move> Solver::solve(Cube &cube) {
     }
 }
 
+class TwoPhaseSolver {
+  private:
+    SinglePhaseSolver solver;
+  public:
+    std::vector<Move> solve(Cube cube) {
+        std::vector<Move> solution = solver.solve<G1>(cube); //Not full solution yet
+        std::cout << "G1 found in " << solution.size() << " moves" << std::endl;
+        for (Move move : solution) { cube.rotate(move); }
+        std::vector<Move> moves_G1_to_solved = solver.solve<SOLVED>(cube);
+        solution.insert(solution.end(), moves_G1_to_solved.begin(), moves_G1_to_solved.end()); //Now it's full solution
+        return solution;
+    }
+};
+
 int main() {
-    Move scramble[] = {R, Up, F, U, Rp, L, U2, B2};
+    Move scramble[] = {U, B2, F2, U, Lp, R2, D2, Rp, D, F2, L2, Rp, U2, Fp, L};
 
     Cube cube = Cube();
     for (Move move : scramble) {
@@ -85,10 +105,13 @@ int main() {
     std::cout << std::endl;
     cube.print();
 
-    Solver solver = Solver();
+    TwoPhaseSolver solver = TwoPhaseSolver();
+    std::cout << "Starting search..." << std::endl;
     std::vector<Move> solution = solver.solve(cube);
     
+    std::cout << "Solution: ";
     for (Move move : solution) {
-        std::cout << move << std::endl;
+        std::cout << move << " ";
     }
+    std::cout << std::endl;
 }
