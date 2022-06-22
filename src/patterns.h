699b78e36dab1550233983841ef4afc40fd70352
Evan Jiang <ecjiang@bluevalleyk12.net>
2022-06-22 00:50:28 -0500

    Fixed some calculations

--- src/patterns.h
+++ src/patterns.h
@@ -91,7 +91,7 @@ class Edge7Hash {
 	std::vector<unsigned int> bitCount; //Look up table for the number of set bits. There are 2^7 possible bit combiniations
 
     template<uint64_t orient_colors>
-    inline uint8_t getOrientation(uint64_t WYRO, uint64_t BGRO) { return (WYRO & orient_colors) ? 1 : 0; }
+    inline uint8_t getOrientation(uint64_t WYRO) { return (WYRO & orient_colors) ? 1 : 0; }
 
   public:
 	Edge7Hash() {
@@ -101,8 +101,8 @@ class Edge7Hash {
         }
         
         //Make the bit count lookup table
-        bitCount.reserve(1 << 7);
-        for (unsigned int i = 0; i < (1 << 7); i++) {
+        bitCount.reserve(1 << 12);
+        for (unsigned int i = 0; i < (1 << 12); i++) {
             uint8_t count = 0;
             unsigned int x = i; // So bits can be removed from x
             if (x) do { count++; } while (x &= x-1);
@@ -154,74 +154,74 @@ class Edge7Hash {
             WYRO = cube.sides[3] >> 32;
             BGRO = cube.sides[1];
             edges[0] = WYRO | BGRO;
-            orient0 = getOrientation<WHITE_YELLOW>(WYRO, BGRO);
+            orient0 = getOrientation<WHITE_YELLOW>(WYRO);
 
             WYRO = cube.sides[3] >> 16;
             BGRO = cube.sides[2];
             edges[1] = WYRO | BGRO;
-            orient1 = getOrientation<WHITE_YELLOW>(WYRO, BGRO);
+            orient1 = getOrientation<WHITE_YELLOW>(WYRO);
 
             WYRO = cube.sides[3];
             BGRO = cube.sides[4];
             edges[2] = WYRO | BGRO;
-            orient2 = getOrientation<WHITE_YELLOW>(WYRO, BGRO);
+            orient2 = getOrientation<WHITE_YELLOW>(WYRO);
 
             WYRO = cube.sides[3] >> 48;
             BGRO = cube.sides[5];
             edges[3] = WYRO | BGRO;
-            orient3 = getOrientation<WHITE_YELLOW>(WYRO, BGRO);
+            orient3 = getOrientation<WHITE_YELLOW>(WYRO);
 
             WYRO = cube.sides[2] >> 48;
             BGRO = cube.sides[1] >> 16;
             edges[4] = WYRO | BGRO;
-            orient4 = getOrientation<RED_ORANGE>(WYRO, BGRO);
+            orient4 = getOrientation<RED_ORANGE>(WYRO);
 
             WYRO = cube.sides[2] >> 16;
             BGRO = cube.sides[4] >> 48;
             edges[5] = WYRO | BGRO;
-            orient5 = getOrientation<RED_ORANGE>(WYRO, BGRO);
+            orient5 = getOrientation<RED_ORANGE>(WYRO);
 
             WYRO = cube.sides[5] >> 48;
             BGRO = cube.sides[4] >> 16;
             edges[6] = WYRO | BGRO;
-            orient6 = getOrientation<RED_ORANGE>(WYRO, BGRO);
+            orient6 = getOrientation<RED_ORANGE>(WYRO);
 
         } else if (selected_edges == 1) {
 
             WYRO = cube.sides[2] >> 16;
             BGRO = cube.sides[4] >> 48;
             edges[0] = WYRO | BGRO;
-            orient0 = getOrientation<RED_ORANGE>(WYRO, BGRO);
+            orient0 = getOrientation<RED_ORANGE>(WYRO);
 
             WYRO = cube.sides[5] >> 48;
             BGRO = cube.sides[4] >> 16;
             edges[1] = WYRO | BGRO;
-            orient1 = getOrientation<RED_ORANGE>(WYRO, BGRO);
+            orient1 = getOrientation<RED_ORANGE>(WYRO);
 
             WYRO = cube.sides[5] >> 16;
             BGRO = cube.sides[1] >> 48;
             edges[2] = WYRO | BGRO;
-            orient2 = getOrientation<RED_ORANGE>(WYRO, BGRO);
+            orient2 = getOrientation<RED_ORANGE>(WYRO);
 
             WYRO = cube.sides[0] >> 32;
             BGRO = cube.sides[1] >> 32;
             edges[3] = WYRO | BGRO;
-            orient3 = getOrientation<WHITE_YELLOW>(WYRO, BGRO);
+            orient3 = getOrientation<WHITE_YELLOW>(WYRO);
 
             WYRO = cube.sides[0] >> 48;
             BGRO = cube.sides[2] >> 32;
             edges[4] = WYRO | BGRO;
-            orient4 = getOrientation<WHITE_YELLOW>(WYRO, BGRO);
+            orient4 = getOrientation<WHITE_YELLOW>(WYRO);
 
             WYRO = cube.sides[0];
             BGRO = cube.sides[4] >> 32;
             edges[5] = WYRO | BGRO;
-            orient5 = getOrientation<WHITE_YELLOW>(WYRO, BGRO);
+            orient5 = getOrientation<WHITE_YELLOW>(WYRO);
 
             WYRO = cube.sides[0] >> 16;
             BGRO = cube.sides[5] >> 32;
             edges[6] = WYRO | BGRO;
-            orient6 = getOrientation<WHITE_YELLOW>(WYRO, BGRO);
+            orient6 = getOrientation<WHITE_YELLOW>(WYRO);
 
         }
 
@@ -273,16 +273,16 @@ class Edge7Hash {
         uint8_t orientation_code = (orient6 << 6) | (orient5 << 5) | (orient4 << 4) | (orient3 << 3) | (orient2 << 2) | (orient1 << 1) | orient0;
         
         //Get Lehmer code of edge permutation
-        uint8_t bit_string = 0;
+        uint16_t bit_string = 0;
         uint32_t edge_code = 0;
         for (int i = 0; i < 7; i++) { 
             uint64_t index = edge_indices[i];
-            bit_string |= uint8_t(0b01000000) >> index;
+            bit_string |= uint16_t(0b100000000000) >> index;
             if (i == 0) {
                 edge_code += index * permutations[i];
                 continue;
             }
-            index -= bitCount[bit_string >> (7 - index)];
+            index -= bitCount[bit_string >> (12 - index)];
             edge_code += index * permutations[i];
         }
         return (edge_code * 128) + orientation_code;
