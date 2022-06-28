4c1a2222cebecf7e524f6523e0dfb0a596510e2f
ecjiang016 <ecjiang016@gmail.com>
2022-06-28 18:59:40 -0500

    Fixed move table to accept Move enum as index

--- src/moves.h
+++ src/moves.h
@@ -5,12 +5,12 @@
 //This file is for generating move tables for the coordniate based cube and not for the regular Rubik's cube moves
 
 namespace MoveTable {
-    extern uint16_t CornerOrient[18][2187];
-    extern uint16_t EdgeOrient[18][2048];
-    extern uint16_t UDSlice[18][495];
-    extern uint16_t CornerPerm[10][40320]; //These 3 are only used in G1 so only 10 moves need to be accounted for
-    extern uint16_t EdgePerm2[10][40320];   
-    extern uint8_t  UDSlice2[10][24]; 
+    extern uint16_t CornerOrient[23][2187];
+    extern uint16_t EdgeOrient[23][2048];
+    extern uint16_t UDSlice[23][495];
+    extern uint16_t CornerPerm[23][40320];
+    extern uint16_t EdgePerm2[23][40320];   
+    extern uint8_t  UDSlice2[23][24]; 
 
     void initalizeTables();
 
