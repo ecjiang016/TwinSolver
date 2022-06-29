6b955c20e1f3dcaf91c24d5920a58bb48826976d
ecjiang016 <ecjiang016@gmail.com>
2022-06-28 19:48:12 -0500

    Added coord_cube_rotation.cpp

---
+++ src/tests/coord_cube_rotation.cpp
@@ -0,0 +1,45 @@
+#include "cube.h"
+#include "moves.h"
+#include "coord.h"
+#include <random>
+#include <assert.h>
+
+int main() {
+    MoveTable::initalizeTables();
+    Cube cube;
+    Coord::Cube coord_cube;
+
+    //Phase 1 coords
+    for (int i = 0; i < 100000; i++) {
+        std::random_device dev;
+        std::mt19937 rng(dev());
+        std::uniform_int_distribution<std::mt19937::result_type> dist18(0, 17);
+
+        Move move = all_moves[dist18(rng)];
+        cube.rotate(move);
+        coord_cube.rotate(move);
+
+        assert(cube.getCornerOrient() == coord_cube.getCornerOrient());
+        assert(cube.getCornerPerm() == coord_cube.getCornerPerm());
+        assert(cube.getEdgeOrient() == coord_cube.getEdgeOrient());
+        assert(cube.getUDSlice() == coord_cube.getUDSlice());
+    }
+
+    //Reset cubes
+    cube = Cube();
+    coord_cube = Coord::Cube();
+
+    //Phase 2 coords
+    for (int i = 0; i < 100000; i++) {
+        std::random_device dev;
+        std::mt19937 rng(dev());
+        std::uniform_int_distribution<std::mt19937::result_type> dist10(0, 9);
+
+        Move move = movesAfterG1[dist10(rng)];
+        cube.rotate(move);
+        coord_cube.rotate(move);
+
+        assert(cube.getEdgePerm2() == coord_cube.getEdgePerm2());
+        assert(cube.getUDSlice2() == coord_cube.getUDSlice2());
+    }
+}
