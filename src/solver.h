9ab90fd4e7353a228e3e19b63a23571391acbd4d
ecjiang016 <ecjiang016@gmail.com>
2022-07-04 15:47:37 -0500

    Changed unsigned ints to uint8_t

--- src/solver.h
+++ src/solver.h
@@ -19,7 +19,7 @@ class TwoPhaseSolver {
     }
     //The phase 2 version is someone down there cause the compiler doesn't want it here
 
-    template<Phase phase, class CUBE> inline uint8_t search(std::vector<Move> &path, unsigned int g, unsigned int bound, CUBE cube) {
+    template<Phase phase, class CUBE> inline uint8_t search(std::vector<Move> &path, uint8_t g, uint8_t bound, CUBE cube) {
         Move last_move = path.back();
         cube.rotate(last_move);
         uint8_t f = g + heuristic<phase, CUBE>(cube);
@@ -37,7 +37,7 @@ class TwoPhaseSolver {
             if ((move & 0b00111) != (last_move & 0b00111)) { // There is no reason why you should move the same layer twice
                 path.push_back(move);
 
-                unsigned int t = search<phase>(path, g + 1, bound, cube);
+                uint8_t t = search<phase, CUBE>(path, g + 1, bound, cube);
                 if (t == FOUND) {
                     return FOUND;
                 } else if (t < min) {
@@ -52,22 +52,22 @@ class TwoPhaseSolver {
         return min;
     }
 
-    template<Phase phase, class CUBE> inline std::vector<Move> IDA_star(CUBE &cube) {
-        unsigned int bound = heuristic<phase, CUBE>(cube);
+    template<Phase phase, class CUBE> inline std::vector<Move> IDA_star(CUBE cube) {
+        uint8_t bound = heuristic<phase, CUBE>(cube);
         std::vector<Move> path;
         path.reserve(30);
         path.push_back(NULL_MOVE); //Placeholder
 
         while (true) {
-            unsigned int t = search<phase, CUBE>(path, 0U, bound, cube);
+            uint8_t t = search<phase, CUBE>(path, uint8_t(0), bound, cube);
             if (t == FOUND) {
                 path.erase(path.begin()); //Delete the placeholder
                 return path;
-            } else if (t == 0xFFFF) {
+            } else if (t == 0xFE) {
                 std::cout << "Not Found" << std::endl;
                 return path;
             }
-
+            
             bound = t;
         }
     }
