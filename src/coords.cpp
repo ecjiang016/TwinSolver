095692275cd4f34e9954c7977d76670ecb2e59d6
ecjiang016 <ecjiang016@gmail.com>
2022-07-17 21:07:56 -0500

    Changed phase 2 coord cube coordinate calculation

--- src/coords.cpp
+++ src/coords.cpp
@@ -20,3 +20,17 @@ void Coords::Phase2::Cube::rotate(Move move) {
     _EdgePerm2 = MoveTable::EdgePerm2[move][_EdgePerm2];
     _UDSlice2 = MoveTable::UDSlice2[move][_UDSlice2];
 }
+
+uint32_t Coords::Phase2::Cube::getCoord() {
+    uint32_t min_sym_coord = 0xFFFFFFFF;
+    for (int i = 0; i < 4; i++) {
+        //Calculate sym_coord which is a combination of the UDSlice2 coord and the EdgePerm2 coord
+        uint32_t sym_coord = (MoveTable::UDRotate::UDSlice2[i][this->_UDSlice2] * 40320) + MoveTable::UDRotate::EdgePerm2[i][this->_EdgePerm2];
+        //Get the minimum
+        if (sym_coord < min_sym_coord) {
+            min_sym_coord = sym_coord;
+        }
+    }
+
+    return (min_sym_coord * 40320) + _CornerPerm;
+}
