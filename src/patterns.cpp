918e17a14e79d66891cda4bab8ee67f2ee3c2a59
ecjiang016 <ecjiang016@gmail.com>
2022-08-10 18:20:45 -0500

    Split pattern build to two different functions

--- src/patterns.cpp
+++ src/patterns.cpp
@@ -15,22 +15,22 @@ struct HashCache {
     inline bool getBit(hash_type hash) { return hashes[hash / 4].getBit(hash % 4); }
 };
 
-template<class CUBE, size_t DatabaseSize, typename hash_type, bool ALL_MOVES>
+template<Phase P = PHASE1>
 void buildDatabase(std::string save_file_name) {
     std::cout << "Building " << save_file_name << "...\n";
     auto start_time = std::chrono::high_resolution_clock::now();
-    std::vector<Nibbles> pattern_depths((DatabaseSize + 1) / 2); //Cut size in half as 2 nibbles are stored together in 1 array element
+    std::vector<Nibbles> pattern_depths((PHASE1_PATTERNS_SIZE + 1) / 2); //Cut size in half as 2 nibbles are stored together in 1 array element
 
     //Using breadth-first search with coord cube
-    HashCache<DatabaseSize, hash_type> hash_cache;
-    std::deque<CUBE> queue; //Storing max 11 moves (5 bit representation each)
+    HashCache<PHASE1_PATTERNS_SIZE, uint32_t> hash_cache;
+    std::deque<Coords::Phase1::Cube> queue; //Storing max 11 moves (5 bit representation each)
     uint64_t next_layer_nodes = 0; //Keeps track of visited nodes of the next depth
     uint8_t depth = 1;
     unsigned int unique_nodes = 1;
-    hash_type hash;
+    uint32_t hash;
     
     //Process the first node
-    CUBE init_cube = CUBE();
+    Coords::Phase1::Cube init_cube = Coords::Phase1::Cube();
     hash = init_cube.getCoord();
     hash_cache.insertHash(hash);
     hash % 2 == 0 ? pattern_depths[hash/2].insertLow(depth) : pattern_depths[hash/2].insertHigh(depth);
@@ -38,13 +38,12 @@ void buildDatabase(std::string save_file_name) {
 
     while (queue.size() != 0) {
         //Take node out of queue
-        CUBE cube = queue.front();
+        Coords::Phase1::Cube cube = queue.front();
         queue.pop_front();
 
         //Add all the nodes from that node to the queue
-        for (int i = 0; i < (ALL_MOVES == true ? 18 : 10); i++) {
-            Move move = ALL_MOVES == true ? all_moves[i] : movesAfterG1[i];
-            CUBE new_cube = cube;
+        for (Move move : all_moves) {
+            Coords::Phase1::Cube new_cube = cube;
             new_cube.rotate(move);
             hash = new_cube.getCoord();
             if (hash_cache.getBit(hash)) { continue; } //Prune if visited before
@@ -76,7 +75,75 @@ void buildDatabase(std::string save_file_name) {
     std::ofstream file;
     file.open("./databases/" + save_file_name, std::ios_base::binary);
     assert(file.is_open());
-    assert(((DatabaseSize + 1) / 2) == pattern_depths.size());
+    assert(((PHASE1_PATTERNS_SIZE + 1) / 2) == pattern_depths.size());
+    std::cout << "Writing depths..." << std::endl;
+    file.write(reinterpret_cast<const char *>(pattern_depths.data()), pattern_depths.size());
+    file.close();
+    std::cout << save_file_name << " has been built ";
+    std::cout << "(" << minutes << " minutes, " << seconds << " seconds)\n" << std::endl;
+}
+
+template<>
+void buildDatabase<PHASE2>(std::string save_file_name) {
+    std::cout << "Building " << save_file_name << "...\n";
+    auto start_time = std::chrono::high_resolution_clock::now();
+    std::vector<Nibbles> pattern_depths((PHASE2_PATTERNS_SIZE + 1) / 2); //Cut size in half as 2 nibbles are stored together in 1 array element
+
+    //Using breadth-first search with coord cube
+    HashCache<PHASE2_PATTERNS_SIZE, uint32_t> hash_cache;
+    std::deque<Coords::Phase2::Cube> queue; //Storing max 11 moves (5 bit representation each)
+    uint64_t next_layer_nodes = 0; //Keeps track of visited nodes of the next depth
+    uint8_t depth = 1;
+    unsigned int unique_nodes = 1;
+    uint32_t hash;
+    
+    //Process the first node
+    Coords::Phase2::Cube init_cube = Coords::Phase2::Cube();
+    hash = init_cube.getCoord();
+    hash_cache.insertHash(hash);
+    hash % 2 == 0 ? pattern_depths[hash/2].insertLow(depth) : pattern_depths[hash/2].insertHigh(depth);
+    queue.push_back(init_cube);
+
+    while (queue.size() != 0) {
+        //Take node out of queue
+        Coords::Phase2::Cube cube = queue.front();
+        queue.pop_front();
+
+        //Add all the nodes from that node to the queue
+        for (Move move : movesAfterG1) {
+            Coords::Phase2::Cube new_cube = cube;
+            new_cube.rotate(move);
+            hash = new_cube.getCoord();
+            if (hash_cache.getBit(hash)) { continue; } //Prune if visited before
+
+            //Add hash to cache and insert it in the pattern database array
+            hash_cache.insertHash(hash);
+            hash % 2 == 0 ? pattern_depths[hash/2].insertLow(depth) : pattern_depths[hash/2].insertHigh(depth);
+
+            queue.push_back(new_cube);
+            next_layer_nodes++;
+            
+        }
+
+        if (queue.size() == next_layer_nodes) {
+            if (queue.size() == 0) { break; }
+            
+            unique_nodes += next_layer_nodes;
+            std::cout << "Finished depth " << int(depth) << ", " << unique_nodes << " unique nodes found" << std::endl;
+            depth++;
+            next_layer_nodes = 0;
+        }
+
+    }
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto minutes = std::chrono::duration_cast<std::chrono::minutes>(end_time - start_time).count();
+    auto seconds = std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time).count() % 60;
+
+    std::ofstream file;
+    file.open("./databases/" + save_file_name, std::ios_base::binary);
+    assert(file.is_open());
+    assert(((PHASE2_PATTERNS_SIZE + 1) / 2) == pattern_depths.size());
     std::cout << "Writing depths..." << std::endl;
     file.write(reinterpret_cast<const char *>(pattern_depths.data()), pattern_depths.size());
     file.close();
@@ -85,7 +152,7 @@ void buildDatabase(std::string save_file_name) {
 }
 
 void buildAllDatabases() {
-    MoveTable::initalizeTables();
-    buildDatabase<Coords::Phase1::Cube, PHASE1_PATTERNS_SIZE, uint32_t, true >("Phase1.patterns");
-    buildDatabase<Coords::Phase2::Cube, PHASE2_PATTERNS_SIZE, uint32_t, false>("Phase2.patterns");
+    MoveTable::initializeTables();
+    //buildDatabase<PHASE1>("Phase1.patterns");
+    buildDatabase<PHASE2>("Phase2.patterns");
 }
